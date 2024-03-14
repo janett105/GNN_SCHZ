eb8781d4d22320ad853f0bc6f047ba61583116a4
jihoo <janett1005@naver.com>
2024-03-14 20:52:22 +0900

    cleanImg

--- code/utils/bandpass_filtering.py
+++ code/utils/bandpass_filtering.py
@@ -2,25 +2,42 @@ from nilearn.image import clean_img
 import pandas as pd
 import numpy as np
 
-def filtering(subj_id):
-    subjects_dir = 'D:/MRI/UCLA_CNP/preprocess/fmriprep/derivatives/'
-
+def filtering(subj_id, subjects_dir):
     input_file = f'{subjects_dir}{subj_id}/func/{subj_id}_task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'
     output_file = f'{subjects_dir}{subj_id}/func/{subj_id}_task-rest_filtered_space-MNI152NLin2009cAsym_desc-preproc_bold.nii'
     mask_file = f'{subjects_dir}{subj_id}/func/{subj_id}_task-rest_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'
     confounds_file = pd.read_csv(f'{subjects_dir}{subj_id}/func/{subj_id}_rest_confounds.csv').values
 
-    np.nan_to_num(confounds_file, copy=False)
+    confounds_file =nan_interp(confounds_file)
 
     filtered_img = clean_img(imgs=input_file,
                             mask_img=mask_file,
                             low_pass=0.1, 
                             high_pass=0.01, 
                             confounds=confounds_file,
-                            t_r=2.0,)
+                            detrend=False,
+                            standardize=False,
+                            t_r=2.0)
 
     filtered_img.to_filename(output_file)
 
-# def create_confounds_file():
+def create_confounds_file(subj_id, subjects_dir):
+    error_subjects=[]
+    try: 
+        input_file = pd.read_csv(f'{subjects_dir}{subj_id}/func/{subj_id}_task-rest_desc-confounds_timeseries.tsv', sep = '\t')
+        print(input_file)
+    except FileNotFoundError as e:  error_subjects.append([{subj_id},e]) 
+    print("error subjects : ", error_subjects)
+    
 
-filtering('sub-10171')
+def nan_interp(file):
+    for col in range(file.shape[1]):
+        y = file[:,col]
+        nans, x= np.isnan(y), lambda z: z.nonzero()[0]
+        y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+        file[:,col] = y
+    return file
+subjects_dir = 'C:/Users/janet/MyProjects/GNN_SCHZ/data'
+#subjects_dir = 'D:/MRI/UCLA_CNP/preprocess/fmriprep/derivatives/'
+#filtering('sub-10171', subjects_dir)
+create_confounds_file('sub-10171', subjects_dir)
--- temp/sadad.py
+++ temp/sadad.py
@@ -1,6 +1,11 @@
 import numpy as np
+import pandas as pd
 
+subj_id = 'sub-10171'
+subjects_dir = 'C:/Users/janet/MyProjects/GNN_SCHZ/data/'
+confounds_file = pd.read_csv(f'{subjects_dir}{subj_id}/func/{subj_id}_rest_confounds.csv').values # (152,13)
 
-y= np.array([1, 1, 1, np.nan, NaN, 2, 2, NaN, 0])
-nans, x= np.isnan(y), lambda z: z.nonzero()[0]
-y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+for col in range(confounds_file.shape[1]):
+    y = confounds_file[:,col]
+    nans, x= np.isnan(y), lambda z: z.nonzero()[0]
+    y[nans]= np.interp(x(nans), x(~nans), y[~nans])
